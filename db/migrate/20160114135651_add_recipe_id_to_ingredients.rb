e670c68e431367e65884a42ee8aa5ee3df123af3
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-25 07:28:29 -0400

    Update migration to squelch warning

--- db/migrate/20160114135651_add_recipe_id_to_ingredients.rb
+++ db/migrate/20160114135651_add_recipe_id_to_ingredients.rb
@@ -1,4 +1,4 @@
-class AddRecipeIdToIngredients < ActiveRecord::Migration
+class AddRecipeIdToIngredients < ActiveRecord::Migration[4.2]
   def change
     add_column :ingredients, :recipe_id, :integer
   end
