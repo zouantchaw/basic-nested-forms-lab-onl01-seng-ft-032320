0d0bcc9e9d22e6d225bd9969a1141df89fa6e0c8
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-25 08:50:40 -0400

    Order specs to match implementation order

--- spec/features/recipe_form_spec.rb
+++ spec/features/recipe_form_spec.rb
@@ -11,10 +11,10 @@ describe "the recipe form", :type => :feature do
 
   it "adds ingredients" do
     visit '/recipes/new'
-    fill_in :recipe_ingredients_attributes_0_quantity, with: '1 cup'
     fill_in :recipe_ingredients_attributes_0_name, with: 'sugar'
-    fill_in :recipe_ingredients_attributes_1_quantity, with: '1 tablespoon'
+    fill_in :recipe_ingredients_attributes_0_quantity, with: '1 cup'
     fill_in :recipe_ingredients_attributes_1_name, with: 'vanilla'
+    fill_in :recipe_ingredients_attributes_1_quantity, with: '1 tablespoon'
     find('input[name="commit"]').click
     expect(Recipe.last.ingredients.map(&:quantity)).to eq ['1 cup', '1 tablespoon']
     expect(Recipe.last.ingredients.map(&:name)).to eq ['sugar', 'vanilla']
